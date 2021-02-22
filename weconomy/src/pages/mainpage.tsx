ea1be1eae4db19e8638a8f66d386c12ab3a32b23
ning1315 <59202849+ning1315@users.noreply.github.com>
2021-02-22 22:52:42 +0900

    set start v2

--- weconomy/src/pages/mainpage.tsx
+++ weconomy/src/pages/mainpage.tsx
@@ -5,11 +5,11 @@ const MainPage: React.FC = () => {
     <div className="mainContainer">
       <div className="section1">
         <div className="textBox">
-          <h1 className="title">
+          <h1 className="MainTitle animate__animated animate__fadeInDown">
             아직도 <span className="textPoint">가계부</span>가 멀게만
             느껴지시나요?
           </h1>
-          <span className="desc">
+          <span className="MainDesc animate__animated animate__fadeInDown">
             Weconomy와 함께 편리한 <span className="textPoint">가계부</span>를
             작성해보세요!
           </span>
@@ -23,9 +23,6 @@ const MainPage: React.FC = () => {
           ></img>
         </div>
       </div>
-      <div className="section2"></div>
-      <div className="section3"></div>
-      <div className="section4"></div>
     </div>
   );
 };
