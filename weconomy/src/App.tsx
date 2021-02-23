6dc5bb8ac262a391989376def62de48bdafc0094
ning1315 <59202849+ning1315@users.noreply.github.com>
2021-02-23 23:48:00 +0900

    set account top graph

--- weconomy/src/App.tsx
+++ weconomy/src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import MainPage from '../src/pages/mainpage';
+import AccountPage from '../src/pages/accountpage';
 import './style/mainpage.scss';
 import './style/LoginModal.scss';
+import './style/AccountPage.scss';
+import './style/AccountGraph.scss';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 
 const App: React.FC = () => {
@@ -10,6 +13,7 @@ const App: React.FC = () => {
       <Switch>
         <Route path="/mainpage" render={() => <MainPage />} />
         <Route exact path="/" render={() => <Redirect to="/mainpage" />} />
+        <Route exact path="/accountpage" render={() => <AccountPage />} />
       </Switch>
     </>
   );
--- weconomy/src/pages/mainpage.tsx
+++ weconomy/src/pages/mainpage.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { successLogin } from '../store/actions/userActions';
 import useMedia from '../customhooks/useMedia';
 import LoginModal from '../component/loginmodal';
 
 const MainPage: React.FC = () => {
   const { isMobile } = useMedia();
 
-  useEffect(() => {
-    console.log(isMobile);
-  });
   return (
     <div className="mainContainer">
       <LoginModal></LoginModal>
       <div className="section1">
         <div className="textBox">
+          {isMobile ? (
+            <h1 className="MainTitleLogo animate__animated animate__fadeInDown">
+              Weconomy
+            </h1>
+          ) : null}
           <h1 className="MainTitle animate__animated animate__fadeInDown">
             아직도 <span className="textPoint">가계부</span>가 멀게만
             느껴지시나요?
@@ -22,7 +26,11 @@ const MainPage: React.FC = () => {
             Weconomy와 함께 편리한 <span className="textPoint">가계부</span>를
             작성해보세요!
           </span>
-          <button className="mainTopBtn">Weconomy 시작하기</button>
+          {isMobile ? (
+            <button className="mainMobileBtn">Weconomy 시작하기</button>
+          ) : (
+            <button className="mainTopBtn">Weconomy 시작하기</button>
+          )}
         </div>
         <div className="imageBox">
           <img
