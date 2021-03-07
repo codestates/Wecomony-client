313111615bb628e0101d066880b7c6ef94fd75c7
ning1315 <59202849+ning1315@users.noreply.github.com>
2021-03-07 21:08:26 +0900

    set delete group modal

--- weconomy/src/component/updateGroupModal.tsx
+++ weconomy/src/component/updateGroupModal.tsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { RootState } from '../store/reducers';
 import TextField from '@material-ui/core/TextField';
+import { RiCloseFill } from 'react-icons/ri';
 import { updateGroupModalClose } from '../store/actions/modalActions';
 import { updateGroupTotalcost } from '../store/actions/groupAction';
 import { useParams } from 'react-router-dom';
@@ -17,6 +18,9 @@ const UpdateGroupModal = () => {
 
   const [newcost, setNewCost] = useState<string>('');
   const [Err, setErr] = useState<string>('');
+  const [isDelete, setIsDelete] = useState<boolean>(false);
+  const [deleteText, setDeleteText] = useState<string>('');
+  const [deleteErr, setDeleteErr] = useState<string>('');
   const isOpen = useSelector(
     (state: RootState) => state.modalStatus.updateGroupModal,
   );
@@ -25,7 +29,12 @@ const UpdateGroupModal = () => {
     setNewCost(e.target.value);
   };
 
+  const getDeleteText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDeleteText(e.target.value);
+  };
+
   const closeModal = () => {
+    setErr('');
     dispatch(updateGroupModalClose());
   };
   const userNow = useSelector((state: RootState) => state.userStatus.userData);
@@ -47,7 +56,24 @@ const UpdateGroupModal = () => {
         groupId: groups[0].id,
         totalcost: newcost,
       };
+      setErr('');
       dispatch(updateGroupTotalcost(data));
+    }
+  };
+
+  const onClickDelete = () => {
+    setIsDelete(true);
+  };
+
+  const cancelDelte = () => {
+    setIsDelete(false);
+  };
+
+  const tryDelete = () => {
+    if (deleteText !== `${groups[0].meetName} 삭제`) {
+      return setDeleteErr('확인 문자와 일치하지 않습니다');
+    } else {
+      console.log('삭제~');
     }
   };
 
@@ -60,38 +86,82 @@ const UpdateGroupModal = () => {
               onClick={(e) => e.stopPropagation()}
               className="insideUpdateGroupModal animate__animated animate__fadeIn"
             >
-              <div className="UpdateGroupModalContents">
-                <div className="UpdateGroupModalTop">
-                  <div className="UpdateGroupModalTitle">Weconomy</div>
+              {!isDelete ? (
+                <div className="UpdateGroupModalContents">
+                  <RiCloseFill
+                    onClick={closeModal}
+                    className="loginModalClose"
+                  ></RiCloseFill>
+                  <div className="UpdateGroupModalTop">
+                    <div className="UpdateGroupModalTitle">Weconomy</div>
+                  </div>
+                  <div className="UpdateGroupModalBody">
+                    <TextField
+                      id="outlined-multiline-flexible"
+                      label="가용 금액 변경"
+                      multiline
+                      value={newcost}
+                      size="small"
+                      onChange={getAccountCost}
+                      fullWidth={true}
+                      style={{ marginBottom: '20px' }}
+                      variant="outlined"
+                    />
+                    {Err ? (
+                      <div className="CreateNewAccountModalErr">{Err}</div>
+                    ) : null}
+                  </div>
+                  <div className="UpdateGroupModalBottom">
+                    <button
+                      onClick={onclickUpdate}
+                      className="UpdateGroupModalBtn"
+                    >
+                      변경하기
+                    </button>
+                    <button
+                      onClick={onClickDelete}
+                      className="DeleteGroupModalBtn"
+                    >
+                      가계부 삭제하기
+                    </button>
+                  </div>
                 </div>
-                <div className="UpdateGroupModalBody">
-                  <TextField
-                    id="outlined-multiline-flexible"
-                    label="가용 금액 변경"
-                    multiline
-                    value={newcost}
-                    size="small"
-                    onChange={getAccountCost}
-                    fullWidth={true}
-                    style={{ marginBottom: '20px' }}
-                    variant="outlined"
-                  />
-                  {Err ? (
-                    <div className="CreateNewAccountModalErr">{Err}</div>
-                  ) : null}
+              ) : (
+                <div className="UpdateGroupModalContents">
+                  <RiCloseFill
+                    onClick={closeModal}
+                    className="loginModalClose"
+                  ></RiCloseFill>
+                  <div className="UpdateGroupModalTop">
+                    <div className="UpdateGroupModalTitle">Weconomy</div>
+                  </div>
+                  <div className="UpdateGroupModalBody">
+                    <input
+                      className="deleteGroupInput"
+                      type="text"
+                      placeholder={`${groups[0].meetName} 삭제`}
+                      value={deleteText}
+                      onChange={getDeleteText}
+                    ></input>
+                    {deleteErr ? (
+                      <div className="CreateNewAccountModalErr">
+                        {deleteErr}
+                      </div>
+                    ) : null}
+                  </div>
+                  <div className="deleteGroupBottom">
+                    <button onClick={tryDelete} className="deleteGroupBtn">
+                      삭제하기
+                    </button>
+                    <button
+                      onClick={cancelDelte}
+                      className="cancelDeleteGroupBtn"
+                    >
+                      취소하기
+                    </button>
+                  </div>
                 </div>
-                <div className="UpdateGroupModalBottom">
-                  <button
-                    onClick={onclickUpdate}
-                    className="UpdateGroupModalBtn"
-                  >
-                    변경하기
-                  </button>
-                  <button onClick={closeModal} className="UpdateGroupModalBtn">
-                    취소하기
-                  </button>
-                </div>
-              </div>
+              )}
             </div>
           </div>
         </div>
