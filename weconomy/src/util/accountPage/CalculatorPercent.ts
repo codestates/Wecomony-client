875112169ac68563a5503b1569a80ea9001ec7d3
ning1315 <59202849+ning1315@users.noreply.github.com>
2021-03-09 19:17:43 +0900

    set delete content

--- weconomy/src/util/accountPage/CalculatorPercent.ts
+++ weconomy/src/util/accountPage/CalculatorPercent.ts
@@ -1,9 +1,12 @@
 
 const CalculatorPercent = (total: number, current: number) => {
-
+    
+ 
   if(current === total){
     return 100
-  }else if(String((current / total)).slice(2,4).length === 1){
+  }else if((current / total) < 0){
+    return 0
+  } else if(String((current / total)).slice(2,4).length === 1){
     return Number(String((current / total)).slice(2,4)) * 10
   }
    else {
